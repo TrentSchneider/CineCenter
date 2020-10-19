f235ec4e3a6976fc8cda55b5243bd9f79e7c423f
Trent Schneider <trentonschneider@gmail.com>
2020-10-19 19:53:51 -0400

    added base header

---
+++ client/src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+// returns the a card for each user with their information filled in
+function Header(props) {
+  return (
+    <header>
+        <h1>CineCenter</h1>
+    </header>
+  );
+}
+
+export default Header;
