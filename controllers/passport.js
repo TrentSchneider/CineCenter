8f6748b603cd2898bec4402af37c07cee016ec9d
Trent Schneider <trentonschneider@gmail.com>
2020-10-19 22:01:07 -0400

    removed old sql version of passport local strategy

--- controllers/passport.js
+++ controllers/passport.js
@@ -4,34 +4,6 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
 passport.use(
-  new LocalStrategy(
-    // Set up using email address to sign in instead of username
-    {
-      usernameField: "email"
-    },
-    (email, password, done) => {
-      db.User.findOne({
-        where: {
-          email: email
-        }
-      }).then(dbUser => {
-        // Validations for incorrect email/password
-        if (!dbUser) {
-          return done(null, false, {
-            message: "Incorrect email."
-          });
-        } else if (!dbUser.validPassword(password)) {
-          return done(null, false, {
-            message: "Incorrect password."
-          });
-        }
-        return done(null, dbUser);
-      });
-    }
-  )
-);
-
-passport.use(
   new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
     User.findOne({ email: email })
       .then(user => {
