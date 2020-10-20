d31229ae2dbaa9bb568c8da017915f30bd86119d
Trent Schneider <trentonschneider@gmail.com>
2020-10-20 08:46:14 -0400

    updated api routes, imports, and exports

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,37 +1,38 @@
 const compression = require("compression");
 const express = require("express");
 const router = express.Router();
-const db = require("../models");
-const passport = require("../config/passport");
+const db = require("../../models");
+const passport = require("../../controllers/passport");
 const app = express();
 
 app.use(compression());
 
 // route for the API login that also uses passport authentication
-router.post("/api/login", passport.authenticate("local"), (req, res) => {
-    res.json({
-      email: req.user.email,
-      id: req.user.id
-    });
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  res.json({
+    email: req.user.email,
+    id: req.user.id
   });
-  
-  // route for API signup
-  router.post("/api/signup", (req, res) => {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
+});
+
+// route for API signup
+router.post("/signup", (req, res) => {
+  db.User.create({
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(() => {
+      res.redirect(307, "/login");
     })
-      .then(() => {
-        res.redirect(307, "/api/login");
-      })
-      .catch(err => {
-        res.status(401).json(err);
-      });
-  });
-  
-  // Route for logging user out
-  router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
-  
+    .catch(err => {
+      res.status(401).json(err);
+    });
+});
+
+// Route for logging user out
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+module.exports = router;
