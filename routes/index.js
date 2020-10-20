9ec8fa9f501432d5bd58ea191532a7268137c018
Trent Schneider <trentonschneider@gmail.com>
2020-10-19 21:55:08 -0400

    updated route to apiRoutes (due to removing index in api routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
-const apiRoutes = require("./api");
+const apiRoutes = require("./api/apiRoutes");
 
 // API Routes
 router.use("/api", apiRoutes);
