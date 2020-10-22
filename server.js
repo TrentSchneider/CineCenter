920fac913c7061ff4bdb7ea9a6bfc8c0c5893ffe
Trent Schneider <trentonschneider@gmail.com>
2020-10-21 20:59:02 -0400

    updated server file with user authentication

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require("express");
-
+const session = require("express-session");
+const passport = require("passport");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport")(passport);
+
 // Add routes, both API and view
 app.use(routes);
 
@@ -19,6 +27,6 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/movieusers");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
