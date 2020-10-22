ad901bc14179fb570fd80529a32bbfc4e8d75a33
Trent Schneider <trentonschneider@gmail.com>
2020-10-21 21:00:58 -0400

    updated user model

--- models/user.js
+++ models/user.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
   {
-    name: {
+    username: {
       type: String,
-      required: true
+      required: true,
+      unique: true
     },
     email: {
       type: String,
