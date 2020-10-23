e52d6989a134d69d0508d7d4f06777e039300e35
Trent Schneider <trentonschneider@gmail.com>
2020-10-23 16:50:42 -0400

    updated passport configuration

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,9 @@
 const bcrypt = require("bcryptjs");
+const passport = require("passport");
 const User = require("../models/user");
 const LocalStrategy = require("passport-local").Strategy;
 
-module.exports = function (passport) {
+module.exports = function () {
   passport.use(
     new LocalStrategy((email, password, done) => {
       User.findOne({ email: email }, (err, user) => {
@@ -33,52 +34,3 @@ module.exports = function (passport) {
     })
   );
 };
-
-// passport.use(
-//   new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-//     User.findOne({ email: email })
-//       .then(user => {
-//         if (!user) {
-//           const newUser = new User({ email, password });
-//           bcrypt.genSalt(10, (err, salt) => {
-//             bcrypt.hash(newUser.password, salt, (err, hash) => {
-//               if (err) throw err;
-//               newUser.password = hash;
-//               newUser
-//                 .save()
-//                 .then(user => {
-//                   return done(null, user);
-//                 })
-//                 .catch(err => {
-//                   return done(null, false, { message: err });
-//                 });
-//             });
-//           });
-//         } else {
-//           bcrypt.compare(password, user.password, (err, isMatch) => {
-//             if (err) throw err;
-//             if (isMatch) {
-//               return done(null, user);
-//             } else {
-//               return done(null, false, { message: "Incorrect password" });
-//             }
-//           });
-//         }
-//       })
-//       .catch(err => {
-//         return done(null, flase, { message: err });
-//       });
-//   })
-// );
-// // Serializes/deserializes the authentication
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser((id,done) => {
-//   User.findById(id, (err, user) => {
-//     done(err, user);
-//   });
-// });
-
-// module.exports = passport;
