2f327de156e4600ab276d1360a81d0b1fc3daeb4
Trent Schneider <trentonschneider@gmail.com>
2020-10-23 16:55:36 -0400

    updated watchlist page to include list components

--- client/src/pages/WatchList/index.js
+++ client/src/pages/WatchList/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import ToWatch from "../../components/ToWatch";
+import Watched from "../../components/Watched";
 
 function WatchList(props) {
-  return <div className="card"></div>;
+  return (
+    <div className="card">
+      <ToWatch data={props.data} />
+      <Watched data={props.data} />
+    </div>
+  );
 }
 
 export default WatchList;
