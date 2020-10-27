7d29f3ee977f04a583dc970d93592cf1be50d7df
Trent Schneider <trentonschneider@gmail.com>
2020-10-26 22:39:35 -0400

    made adjustments to passport config file

--- config/passport.js
+++ config/passport.js
@@ -1,36 +1,42 @@
 const bcrypt = require("bcryptjs");
-const passport = require("passport");
 const User = require("../models/user");
+const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
-module.exports = function () {
-  passport.use(
-    new LocalStrategy((email, password, done) => {
-      User.findOne({ email: email }, (err, user) => {
-        if (err) throw err;
-        if (!user) return done(null, false);
-        bcrypt.compare(password, user.password, (err, res) => {
-          if (err) throw err;
-          if (results === true) {
-            return done(null, user);
-          } else {
-            return done(null, false);
-          }
+passport.use(
+  new LocalStrategy({usernameField:"email"},(email, password, done) => {
+    User.findOne({ email: email }, (err, user) => {
+      if (err) throw err;
+      if (!user)
+        return done(null, false, {
+          message: "incorrect email"
         });
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) throw err;
+        if (res === true) {
+          return done(null, user, { message: "valid username/password" });
+        } else {
+          return done(null, false, { message: "incorrect password" });
+        }
       });
-    })
-  );
+    });
+  })
+);
 
-  passport.serializeUser((user, cb) => {
-    cb(null, user.id);
-  });
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
 
-  passport.deserializeUser((id, cb) =>
-    User.findOne({ _id: id }, (err, user) => {
-      const userInformation = {
-        username: user.username
-      };
-      cb(err, userInformation);
-    })
-  );
-};
+passport.deserializeUser((id, cb) =>
+  User.findById(id, (err, user) => {
+    const userInformation = {
+      username: user.username
+    };
+    // const userInformation = {
+    //   username: "dummyname"
+    // };
+    cb(err, userInformation);
+  })
+);
+
+module.exports = passport;
