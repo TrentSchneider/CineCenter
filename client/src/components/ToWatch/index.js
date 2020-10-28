7b314b21e7eccf5d21444bebf870436c879697c3
Trent Schneider <trentonschneider@gmail.com>
2020-10-27 23:41:06 -0400

    replaced placeholder array with correct array

--- client/src/components/ToWatch/index.js
+++ client/src/components/ToWatch/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function ToWatch(props) {
+  console.log("props.toWatchList", props.toWatchList)
   return (
     <div className="card col-5">
-      {props.data.list.towatch.map((data, i) => (
+      <h1>To Watch List</h1>
+      {props.toWatchList.map((data, i) => (
         <div className="card" key={i}>
           <p>Title: {data.Title}</p>
           <img src={data.Poster} alt={data.Title} />
