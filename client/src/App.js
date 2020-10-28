9cd75de13a683fa57e77b658493a7d566b056188
Trent Schneider <trentonschneider@gmail.com>
2020-10-27 23:45:24 -0400

    updated prop states, added MovieInfo page to routes and transfered user data to pages

--- client/src/App.js
+++ client/src/App.js
@@ -10,22 +10,25 @@ import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
 import WatchList from "./pages/WatchList";
 import MovieSearch from "./pages/MovieSearch";
-import MovieInfo from "./pages/MovieInfo"
+import MovieInfo from "./pages/MovieInfo";
 import API from "./utils/API";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [data, setData] = useState(null);
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("test3@test.com");
+  const [loginPassword, setLoginPassword] = useState("password");
   const [registerUsername, setRegisterUsername] = useState("");
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [searchMovie, setSearchMovie] = useState("");
+  const [toWatchList, setToWatchList] = useState([]);
+  const [watchedList, setWatchedList] = useState([]);
 
   useEffect(() => {
     // window.location.replace(process.env.PUBLIC_URL + "/home");
+    console.log("user data", data);
   }, [isLoggedIn]);
 
   function handleLogoutClick() {
@@ -34,6 +37,15 @@ function App() {
   function handleLoginClick(event) {
     event.preventDefault();
     API.login(loginEmail, loginPassword).then(res => {
+      console.log("res response", res);
+      console.log("res data list", res.data.list);
+      console.log("res data list towatch", res.data.list.towatch);
+      setData(res.data);
+      setToWatchList(res.data.list.towatch);
+      console.log("to watch", toWatchList);
+      setWatchedList(res.data.list.watched);
+      console.log("watched list", watchedList);
+      console.log("data from login", data);
       setIsLoggedIn(true);
     });
   }
@@ -58,9 +70,10 @@ function App() {
   if (isLoggedIn) {
     console.log("is logged in", isLoggedIn);
 
-    API.getUser().then(res => {
-      setData(res.data);
-    });
+    // API.getUser().then(res => {
+    //   setData(res.data);
+    //   console.log("res.data",res.data);
+    // });
     return (
       <Router>
         <Header data={data} />
@@ -71,7 +84,7 @@ function App() {
               <Home data={data} />
             </Route>
             <Route exact path="/watchlist">
-              <WatchList data={data} />
+              <WatchList toWatchList={toWatchList} watchedList={watchedList} />
             </Route>
           </Switch>
           <Route exact path="/moviesearch">
@@ -79,6 +92,13 @@ function App() {
               searchResult={searchResult}
               setSearchMovie={setSearchMovie}
               handleSearchClick={handleSearchClick}
+            />
+          </Route>
+          <Route exact path="/movie">
+            <MovieInfo
+              searchResult={searchResult}
+              watchedList={watchedList}
+              toWatchList={toWatchList}
             />
           </Route>
         </div>
@@ -119,7 +139,11 @@ function App() {
               />
             </Route>
             <Route exact path="/movie">
-              <MovieInfo searchResult={searchResult} />
+              <MovieInfo
+                searchResult={searchResult}
+                watchedList={watchedList}
+                toWatchList={toWatchList}
+              />
             </Route>
           </Switch>
         </div>
