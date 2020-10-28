7ea94ccadda008ce15f3aa9952ad01cc5506dd05
Trent Schneider <trentonschneider@gmail.com>
2020-10-27 22:25:15 -0400

    added logic to either insert a button to add a movie to the watch list if it's not on the user's watch list or insert a button that says the movie is already on the user's watch list if it is already on there

--- client/src/pages/MovieInfo/index.js
+++ client/src/pages/MovieInfo/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function MovieInfo(props) {
-    console.log("movie info", props.searchResult)
+  console.log("movie info", props.searchResult);
+  let addBtn;
+  props.toWatchList.forEach(e => {
+    if (props.searchResult.Title === e) {
+      addBtn = <button>Already on Watch List</button>;
+    }
+  });
+  if (addBtn === undefined) {
+    addBtn = <button>Add to Watch List</button>;
+  }
   return (
     <div className="card col-8">
       <div className="card">
@@ -9,6 +18,7 @@ function MovieInfo(props) {
         <img src={props.searchResult.Poster} alt={props.searchResult.Title} />
         <p>Year: {props.searchResult.Year}</p>
         <p>Description: {props.searchResult.Plot}</p>
+        {addBtn}
       </div>
     </div>
   );
