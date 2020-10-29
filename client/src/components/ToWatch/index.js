701808d2f3646a8830995037ecfdb2e92bb89824
Trent Schneider <trentonschneider@gmail.com>
2020-10-28 22:36:20 -0400

    updated state reference for array and added button

--- client/src/components/ToWatch/index.js
+++ client/src/components/ToWatch/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 function ToWatch(props) {
-  console.log("props.toWatchList", props.toWatchList)
+  console.log("props.toWatchList", props.user.towatch);
   return (
     <div className="card col-5">
       <h1>To Watch List</h1>
-      {props.toWatchList.map((data, i) => (
+      {props.user.towatch.map((data, i) => (
         <div className="card" key={i}>
           <p>Title: {data.Title}</p>
           <img src={data.Poster} alt={data.Title} />
           <p>Year: {data.Year}</p>
           <p>Description: {data.Plot}</p>
+          <button
+            onClick={() =>
+              props.handleMoveToWatched(props.user.id, data.Title, data.Poster)
+            }
+          >
+            Move to Watched List
+          </button>
         </div>
       ))}
     </div>
