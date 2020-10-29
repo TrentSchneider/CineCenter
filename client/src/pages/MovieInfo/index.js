6e8d5ada4d8acf9553a1ec0e6de053e29a511c45
Trent Schneider <trentonschneider@gmail.com>
2020-10-28 22:37:17 -0400

    swapped states that hold arrays and added button

--- client/src/pages/MovieInfo/index.js
+++ client/src/pages/MovieInfo/index.js
@@ -2,14 +2,15 @@ import React from "react";
 
 function MovieInfo(props) {
   console.log("movie info", props.searchResult);
+  console.log("user", props.user)
   let addBtn;
-  props.toWatchList.forEach(e => {
+  props.user.towatch.forEach(e => {
     if (props.searchResult.Title === e) {
       addBtn = <button>Already on Watch List</button>;
     }
   });
   if (addBtn === undefined) {
-    addBtn = <button>Add to Watch List</button>;
+    addBtn = <button onClick={props.handleAddToWatch}>Add to Watch List</button>;
   }
   return (
     <div className="card col-8">
