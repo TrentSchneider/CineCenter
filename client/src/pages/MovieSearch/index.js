f8657f6ac1f332f1c86557f21a816f21ba93f05c
Trent Schneider <trentonschneider@gmail.com>
2020-11-01 15:31:33 -0500

    moved states and functions only needed by this page to the page instead of passing them from App

--- client/src/pages/MovieSearch/index.js
+++ client/src/pages/MovieSearch/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieResult from "../../components/MovieResult";
 
 function MovieSearch(props) {
+  const [searchMovie, setSearchMovie] = useState("");
+
+  function handleSearchClick(event) {
+    event.preventDefault();
+    props.API.searchMovie(searchMovie)
+      .then(res => {
+        props.setSearchResult(res.data);
+      })
+      .catch(err => console.log("err", err));
+  }
+
   console.log("data", props.searchResult);
   return (
     <div className="d-flex justify-content-center">
@@ -15,7 +26,7 @@ function MovieSearch(props) {
               id="inputMovie"
               aria-describedby="inputMovieHelp"
               placeholder="Movie Title"
-              onChange={e => props.setSearchMovie(e.target.value)}
+              onChange={e => setSearchMovie(e.target.value)}
             />
             <small id="inputMovieHelp" className="form-text text-muted">
               Search:
@@ -24,7 +35,7 @@ function MovieSearch(props) {
           <button
             type="submit"
             className="btn btn-primary"
-            onClick={props.handleSearchClick}
+            onClick={handleSearchClick}
           >
             Submit
           </button>
