f6364c421ffd92f8654789b171ace434154d3f10
Trent Schneider <trentonschneider@gmail.com>
2020-11-01 15:28:13 -0500

    added property to function

--- client/src/pages/MovieInfo/index.js
+++ client/src/pages/MovieInfo/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function MovieInfo(props) {
   console.log("movie info", props.searchResult);
-  console.log("user", props.user)
+  console.log("user", props.user);
   let addBtn;
   props.user.towatch.forEach(e => {
     if (props.searchResult.Title === e) {
@@ -10,8 +10,17 @@ function MovieInfo(props) {
     }
   });
   if (addBtn === undefined) {
-    addBtn = <button onClick={props.handleAddToWatch}>Add to Watch List</button>;
+    addBtn = (
+      <button
+        onClick={() => {
+          props.handleAddToWatch(props.searchResult);
+        }}
+      >
+        Add to Watch List
+      </button>
+    );
   }
+
   return (
     <div className="card col-8">
       <div className="card">
