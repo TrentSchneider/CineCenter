9554d9e57712c0cd2fc548d3737d841089cc06ff
Trent Schneider <trentonschneider@gmail.com>
2020-11-01 15:26:33 -0500

    moved local states and functions from App to login

--- client/src/pages/LogIn/index.js
+++ client/src/pages/LogIn/index.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LogIn(props) {
+  const [loginEmail, setLoginEmail] = useState("test4@test.com");
+  const [loginPassword, setLoginPassword] = useState("password");
+
+  function handleLoginClick(event) {
+    event.preventDefault();
+    props.API.login(loginEmail, loginPassword).then(res => {
+      console.log(res.data);
+      props.setUser({
+        id: res.data._id,
+        email: res.data.email,
+        username: res.data.username,
+        towatch: res.data.towatch,
+        watched: res.data.watched
+      });
+      console.log("user after login", props.user);
+      props.setIsLoggedIn(true);
+      
+    });
+  }
+
   return (
     <div className="container">
       <div className="card">
@@ -12,7 +32,7 @@ function LogIn(props) {
               className="form-control"
               id="emailInput"
               placeholder="Email"
-              onChange={e => props.setLoginEmail(e.target.value)}
+              onChange={e => setLoginEmail(e.target.value)}
             />
           </div>
           <div className="form-group">
@@ -22,13 +42,10 @@ function LogIn(props) {
               className="form-control"
               id="passwordInput"
               placeholder="Password"
-              onChange={e => props.setLoginPassword(e.target.value)}
+              onChange={e => setLoginPassword(e.target.value)}
             />
           </div>
-          <button
-            className="btn btn-default"
-            onClick={props.handleLoginClick}
-          >
+          <button className="btn btn-default" onClick={handleLoginClick}>
             Login
           </button>
         </form>
