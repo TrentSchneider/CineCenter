75689fd452c950ce4f8d5dc7049ea10f580bf4d6
Trent Schneider <trentonschneider@gmail.com>
2020-11-03 13:29:56 -0500

    moved functions/states specific to individual pages to their respective pages, corrected user info grab, and set dummy info for user

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
 import Header from "./components/Header";
 import SignedInNav from "./components/Nav/SignedInNav";
 import SignedOutNav from "./components/Nav/SignedOutNav";
@@ -11,161 +16,184 @@ import SignUp from "./pages/SignUp";
 import WatchList from "./pages/WatchList";
 import MovieSearch from "./pages/MovieSearch";
 import MovieInfo from "./pages/MovieInfo";
+import Map from "./pages/Map";
 import API from "./utils/API";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [loginEmail, setLoginEmail] = useState("test3@test.com");
-  const [loginPassword, setLoginPassword] = useState("password");
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
+  const [user, setUser] = useState({
+    id: "testID",
+    email: "testEmail",
+    username: "testUsername",
+    towatch: [{ Title: "test1" }, { Title: "test2" }],
+    watched: [{ Title: "test4" }, { Title: "test4" }]
+  });
   const [searchResult, setSearchResult] = useState([]);
-  const [searchMovie, setSearchMovie] = useState("");
-  const [addToWatch, setAddToWatch] = useState([]);
-  const [moveMovie, setMoveMovie] = useState([]);
-  const [user, setUser] = useState([]);
-  // let user=[]
+  const [selectedResult, setSelectedResult] = useState("");
+
   useEffect(() => {
-    console.log("user after login", user);
-  }, [isLoggedIn, user]);
+    userInfo();
+  }, [isLoggedIn]);
+  useEffect(() => {
+    console.log("user updated", user);
+    console.log("user id", user.id);
+  }, [user]);
 
   function handleLogoutClick() {
-    setIsLoggedIn(false);
+    API.logout().then(() => {
+      setIsLoggedIn(false);
+    });
   }
-  function handleLoginClick(event) {
-    event.preventDefault();
-    API.login(loginEmail, loginPassword).then(res => {
-      console.log(res.data);
 
+  function handleAddToWatch(data) {
+    // setAddToWatch(user.id,searchResult.Title,searchResult.Poster );
+    API.addToWatch(data).then(res => {
+      console.log("added to towatch", res.data);
+    });
+  }
+
+  function handleMoveToWatched(data) {
+    API.moveToWatch(data).then(res => {
+      console.log("moved from towatch to watched", res.data);
+      // userInfo();
+    });
+  }
+
+  function handleDeleteToWatch(data) {
+    API.deleteToWatch(data).then(res => {
+      console.log("removed from towatch", res.data);
+      // userInfo();
+    });
+  }
+
+  function handleDeleteWatched(data) {
+    API.deleteWatched(data).then(res => {
+      console.log("removed from watched", res.data);
+    });
+  }
+
+  function userInfo() {
+    API.getUser().then(res => {
+      console.log("get user response", res.data);
       setUser({
         id: res.data._id,
+        email: res.data.email,
         username: res.data.username,
         towatch: res.data.towatch,
         watched: res.data.watched
       });
-      console.log("user after login", user);
-
-      setIsLoggedIn(true);
-    });
-  }
-  function handleRegisterClick(event) {
-    event.preventDefault();
-    API.register(registerUsername, registerEmail, registerPassword).then(() => {
-      API.login(registerEmail, registerPassword).then(() => {
+      if (res.data.user) {
         setIsLoggedIn(true);
-      });
-    });
-  }
-
-  function handleSearchClick(event) {
-    event.preventDefault();
-    API.searchMovie(searchMovie)
-      .then(res => {
-        setSearchResult(res.data);
-      })
-      .catch(err => console.log("err", err));
-  }
-
-  function handleAddToWatch(event) {
-    event.preventDefault();
-    // setAddToWatch(user.id,searchResult.Title,searchResult.Poster );
-    API.addToWatch(user.id, searchResult.Title, searchResult.Poster).then(
-      res => {
-        console.log("added to towatch", res.data);
       }
-    );
-  }
-
-  function handleMoveToWatched(id, title, poster) {
-    API.addToWatched(id, title, poster).then(() => {
-      // API.deleteToWatch(id, title).then(res => {
-      //   console.log("moved to watched", res.data);
-      // });
     });
   }
 
   if (isLoggedIn) {
     console.log("is logged in", isLoggedIn);
-
-    // API.getUser().then(res => {
-    //   setData(res.data);
-    //   console.log("res.data",res.data);
-    // });
     return (
-      <Router>
-        <Header user={user} />
-        <SignedInNav handleLogoutClick={handleLogoutClick} />
-        <div className="layer">
-          <Switch>
-            <Route exact path={["/", "/home"]}>
-              <Home user={user} />
-            </Route>
-            <Route exact path="/watchlist">
-              <WatchList
-                user={user}
-                handleMoveToWatched={handleMoveToWatched}
-              />
-            </Route>
-          </Switch>
-          <Route exact path="/moviesearch">
-            <MovieSearch
-              searchResult={searchResult}
-              setSearchMovie={setSearchMovie}
-              handleSearchClick={handleSearchClick}
-            />
-          </Route>
-          <Route exact path="/movie">
-            <MovieInfo
-              searchResult={searchResult}
-              user={user}
-              handleAddToWatch={handleAddToWatch}
-            />
-          </Route>
-        </div>
-        <Footer />
-      </Router>
+      <div className="backC">
+        <Router>
+          <Header user={user} />
+          <SignedInNav handleLogoutClick={handleLogoutClick} />
+          <div className="layer">
+            <Switch>
+              <Route exact path={["/", "/home"]}>
+                <Home user={user} />
+              </Route>
+              <Route exact path="/watchlist">
+                <WatchList
+                  user={user}
+                  handleDeleteToWatch={handleDeleteToWatch}
+                  handleMoveToWatched={handleMoveToWatched}
+                  handleDeleteWatched={handleDeleteWatched}
+                />
+              </Route>
+              <Route exact path="/moviesearch">
+                <MovieSearch
+                  API={API}
+                  setSearchResult={setSearchResult}
+                  searchResult={searchResult}
+                  setSelectedResult={setSelectedResult}
+                />
+              </Route>
+              <Route exact path="/movie/:id">
+                <MovieInfo
+                  searchResult={searchResult}
+                  user={user}
+                  handleAddToWatch={handleAddToWatch}
+                  userInfo={userInfo}
+                  selectedResult={selectedResult}
+                  API={API}
+                />
+              </Route>
+              <Route exact path="/map">
+                <Map />
+              </Route>
+            </Switch>
+          </div>
+          <Footer />
+        </Router>
+      </div>
     );
   } else {
     console.log("is logged in", isLoggedIn);
     return (
-      <Router>
-        <Header />
-        <SignedOutNav />
-        <div className="layer">
-          <Switch>
-            <Route exact path={["/", "/home"]}>
-              <Home />
-            </Route>
-            <Route exact path="/login">
-              <LogIn
-                handleLoginClick={handleLoginClick}
-                setLoginEmail={setLoginEmail}
-                setLoginPassword={setLoginPassword}
-              />
-            </Route>
-            <Route exact path="/signup">
-              <SignUp
-                handleRegisterClick={handleRegisterClick}
-                setRegisterUsername={setRegisterUsername}
-                setRegisterEmail={setRegisterEmail}
-                setRegisterPassword={setRegisterPassword}
-              />
-            </Route>
+      <div className="backC">
+        <Router>
+          <Header />
+          <SignedOutNav />
+          <div className="layer backH">
+            <Switch>
+              <Route exact path={["/", "/home"]}>
+                <Home />
+              </Route>
+              <Route exact path="/login">
+                <LogIn
+                  // handleLoginClick={handleLoginClick}
+                  // setLoginEmail={setLoginEmail}
+                  // setLoginPassword={setLoginPassword}
+                  API={API}
+                  user={user}
+                  setUser={setUser}
+                  setIsLoggedIn={setIsLoggedIn}
+                />
+              </Route>
+              <Route exact path="/signup">
+                <SignUp
+                  API={API}
+                  user={user}
+                  setUser={setUser}
+                  setIsLoggedIn={setIsLoggedIn}
+                />
+              </Route>
+            </Switch>
+
             <Route exact path="/moviesearch">
               <MovieSearch
+                API={API}
+                setSearchResult={setSearchResult}
                 searchResult={searchResult}
-                setSearchMovie={setSearchMovie}
-                handleSearchClick={handleSearchClick}
+                setSelectedResult={setSelectedResult}
               />
             </Route>
-            <Route exact path="/movie">
-              <MovieInfo searchResult={searchResult} />
+            <Route exact path="/movie/:id">
+              <MovieInfo
+                searchResult={searchResult}
+                user={user}
+                handleAddToWatch={handleAddToWatch}
+                userInfo={userInfo}
+                selectedResult={selectedResult}
+                API={API}
+                isLoggedIn={isLoggedIn}
+              />
             </Route>
-          </Switch>
-        </div>
-        <Footer />
-      </Router>
+            <Route exact path="/map">
+              <Map />
+            </Route>
+          </div>
+          <Footer />
+        </Router>
+      </div>
     );
   }
 }
