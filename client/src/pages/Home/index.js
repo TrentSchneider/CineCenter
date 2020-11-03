d6b481be2d995ce1b5cde91167da58c0e8fe0f32
Trent Schneider <trentonschneider@gmail.com>
2020-11-03 13:31:25 -0500

    added css

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function Home(props) {
-  console.log("data", props.data);
+  console.log("home user", props.user);
   return (
-    <div className="d-flex justify-content-center">
-      <div className="card col-8"></div>
+    <div className="d-flex justify-content-center backC minH-600">
+      <div className="card col-8 minH-8">
+        <h1 className="bf">Placeholder</h1>
+      </div>
     </div>
   );
 }
