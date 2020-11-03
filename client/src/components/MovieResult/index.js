6d2ec3713a8c9fe77acb27f5f95083c9c1b8cfbd
Trent Schneider <trentonschneider@gmail.com>
2020-11-02 23:57:23 -0500

    updated transition to detailed result page

--- client/src/components/MovieResult/index.js
+++ client/src/components/MovieResult/index.js
@@ -5,40 +5,29 @@ function MovieResult(props) {
   console.log("movie component data", props.searchResult);
   if (props.searchResult.length === 0) {
     return (
-      <div className="card col-5">
+      <div className="card col-12">
         <h3>No Results</h3>
       </div>
     );
-  } else if (props.searchResult.length > 1) {
+  } else {
     return (
-      <div className="card col-5">
-        {props.searchResult.map((data, i) => (
-          <div className="card" key={i}>
-            <p>Title: {data.title}</p>
-            <img src={data.poster} alt={data.title} />
-          </div>
+      <div className="card col-12">
+        {props.searchResult.Search.map((data, i) => (
+          <Link
+            to={{
+              pathname: "/movie/" + data.imdbID,
+              state: { isLoggedIn: true, selectedResult: true }
+            }}
+            onClick={props.setSelectedResult(data.imdbID)}
+            key={i}
+          >
+            <div className="card" data-id={data.imdbID}>
+              <p>Title: {data.Title}</p>
+              <img src={data.Poster} alt={data.Title} />
+            </div>
+          </Link>
         ))}
       </div>
-    );
-  } else {
-    console.log("title", props.searchResult.Title);
-    return (
-      <Link
-        to={{
-          pathname: "/movie",
-          state: { isLoggedIn: true, searchResult: true }
-        }}
-      >
-        <div className="card col-5">
-          <div className="card">
-            <p>Title: {props.searchResult.Title}</p>
-            <img
-              src={props.searchResult.Poster}
-              alt={props.searchResult.Title}
-            />
-          </div>
-        </div>
-      </Link>
     );
   }
 }
