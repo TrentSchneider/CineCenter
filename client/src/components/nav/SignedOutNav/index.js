4335026ce74f2b7eca1fb159281a761d4398daab
Trent Schneider <trentonschneider@gmail.com>
2020-11-03 13:30:56 -0500

    added user state to links

--- client/src/components/nav/SignedOutNav/index.js
+++ client/src/components/nav/SignedOutNav/index.js
@@ -11,31 +11,49 @@ function DropMenu(props) {
       <div className="card-body content">
         <ul className="p-0">
           <li>
-            <Link to={{ pathname: "/home", state: { isLoggedIn: true } }}>
+            <Link
+              to={{
+                pathname: "/home",
+                state: { isLoggedIn: true, user: true }
+              }}
+            >
               Home
             </Link>
           </li>
           <li>
             <Link
-              to={{ pathname: "/moviesearch", state: { isLoggedIn: true } }}
+              to={{
+                pathname: "/moviesearch",
+                state: { isLoggedIn: true, user: true }
+              }}
             >
               Movie Search
             </Link>
           </li>
           <li>
             <Link
-              to={{ pathname: "/map", state: { isLoggedIn: true } }}
+              to={{ pathname: "/map", state: { isLoggedIn: true, user: true } }}
             >
               Find Nearby Theaters
             </Link>
           </li>
           <li>
-            <Link to={{ pathname: "/login", state: { isLoggedIn: true } }}>
+            <Link
+              to={{
+                pathname: "/login",
+                state: { isLoggedIn: true, user: true }
+              }}
+            >
               Sign In
             </Link>
           </li>
           <li>
-            <Link to={{ pathname: "/signup", state: { isLoggedIn: true } }}>
+            <Link
+              to={{
+                pathname: "/signup",
+                state: { isLoggedIn: true, user: true }
+              }}
+            >
               Sign Up
             </Link>
           </li>
