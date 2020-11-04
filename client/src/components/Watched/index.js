78f372c37f2eb351c87ce9668bdc71a7e00212bf
Trent Schneider <trentonschneider@gmail.com>
2020-11-03 22:41:02 -0500

    switched out user for lists for list calls and started groundwork for btnClick state change for re-render

--- client/src/components/Watched/index.js
+++ client/src/components/Watched/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 
 function Watched(props) {
-  console.log("props.watchedList", props.user.watched);
+  console.log("props.watchedList", props.lists.watched);
   return (
     <div className="card col-5">
       <h1>Watched List</h1>
-      {props.user.watched.map((data, i) => (
+      {props.lists.watched.map((data, i) => (
         <div className="card" key={i}>
           <p>Title: {data.Title}</p>
           <img src={data.Poster} alt={data.Title} />
-           <button
+          <button
             onClick={() => {
               props.handleDeleteToWatch(props.user.id, data);
+              if (props.btnClick) {
+                props.setBtnClick(false);
+              } else {
+                props.setBtnClick(true);
+              }
             }}
           >
             Delete
