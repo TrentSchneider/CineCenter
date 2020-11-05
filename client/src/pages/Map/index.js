ecee05febc1794cc133f863fdc2b08627cc23fcb
Trent Schneider <trentonschneider@gmail.com>
2020-11-05 09:48:29 -0500

    commented out non functioning piece of map

--- client/src/pages/Map/index.js
+++ client/src/pages/Map/index.js
@@ -15,15 +15,14 @@ class GoogleMap extends Component {
 
   componentDidMount() {
     const googleMapScript = document.createElement("script");
-
     googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API}&libraries=places`;
 
     window.document.body.appendChild(googleMapScript);
 
-    googleMapScript.addEventListener("load", () => {
-      this.googleMap = this.createGoogleMap();
-      this.marker = this.createMarker();
-    });
+    // googleMapScript.addEventListener("load", () => {
+    //   this.googleMap = this.createGoogleMap();
+    //   this.marker = this.createMarker();
+    // });
   }
 
   createGoogleMap = () => {
@@ -38,46 +37,46 @@ class GoogleMap extends Component {
       radius: "8047",
       types: ["car_repair"]
     };
-    // uses the query to search near the map location and adds markers to the results
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
-    // the function to add markers to all result locations
-    function callback(results, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        console.log(results);
-        for (var i = 0; i < results.length; i++) {
-          createMarker(results[i]);
-        }
-      }
-      // the function to create the markers and attach them to the infowindows
-      function createMarker(place) {
-        const infowindow = new google.maps.InfoWindow();
+    // // uses the query to search near the map location and adds markers to the results
+    // var service = new google.maps.places.PlacesService(map);
+    // service.nearbySearch(request, callback);
+    // // the function to add markers to all result locations
+    // function callback(results, status) {
+    //   if (status == google.maps.places.PlacesServiceStatus.OK) {
+    //     console.log(results);
+    //     for (var i = 0; i < results.length; i++) {
+    //       createMarker(results[i]);
+    //     }
+    //   }
+    //   // the function to create the markers and attach them to the infowindows
+    //   function createMarker(place) {
+    //     const infowindow = new google.maps.InfoWindow();
 
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-        google.maps.event.addListener(marker, "click", function () {
-          var urlName = place.name,
-            formName = urlName.replace(/\s/g, "+");
-          // inserts the location's name, address, rating, and link to driving directions to the location from the users's location
-          var windowContent = `<div id="content"> 
-            <div id="siteNotice"> 
-            </div>
-            <h5 id="firstHeading" class="firstHeading">${place.name}</h5>
-            <div id="bodyContent">
-            <p>Address: ${place.vicinity}</p>
-            <p>Average Rating: ${place.rating} out of 
-            ${place.user_ratings_total} reviews</p>" +
-            <a href="https://www.google.com/maps/dir/?api=1&destination=${formName}&destination_place_id=${place.place_id}>Get Directions</a>
-            </div>
-             </div>`;
-          infowindow.setContent(windowContent);
-          infowindow.open(map, this);
-        });
-      }
-    }
+    //     var placeLoc = place.geometry.location;
+    //     var marker = new google.maps.Marker({
+    //       map: map,
+    //       position: place.geometry.location
+    //     });
+    //     google.maps.event.addListener(marker, "click", function () {
+    //       var urlName = place.name,
+    //         formName = urlName.replace(/\s/g, "+");
+    //       // inserts the location's name, address, rating, and link to driving directions to the location from the users's location
+    //       var windowContent = `<div id="content"> 
+    //         <div id="siteNotice"> 
+    //         </div>
+    //         <h5 id="firstHeading" class="firstHeading">${place.name}</h5>
+    //         <div id="bodyContent">
+    //         <p>Address: ${place.vicinity}</p>
+    //         <p>Average Rating: ${place.rating} out of 
+    //         ${place.user_ratings_total} reviews</p>" +
+    //         <a href="https://www.google.com/maps/dir/?api=1&destination=${formName}&destination_place_id=${place.place_id}>Get Directions</a>
+    //         </div>
+    //          </div>`;
+    //       infowindow.setContent(windowContent);
+    //       infowindow.open(map, this);
+    //     });
+    //   }
+    // }
   };
 
   render() {
