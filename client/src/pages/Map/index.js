b8f7d8f5d02aed914aae911cc85c38ec6cb64187
Trent Schneider <trentonschneider@gmail.com>
2020-11-15 00:59:15 -0500

    added in .env key reference instead of using local key reference.

--- client/src/pages/Map/index.js
+++ client/src/pages/Map/index.js
@@ -30,8 +30,8 @@ const MyMapComponent = compose(
   withProps({
     googleMapURL:
       "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=" +
-      // ,
-      API_PW,
+      process.env.REACT_APP_API,
+    // API_PW,
 
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
