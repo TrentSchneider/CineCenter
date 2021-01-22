2743837eb3773462d7541ace5b74e1733769c393
TrentSchneider <trentonschneider@gmail.com>
2021-01-22 16:37:26 -0500

    added initial maintenance page

---
+++ client/src/pages/Maintenance/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Maintenance(props) {
+  return (
+    <div className="d-flex justify-content-center minH-600 pb-5">
+      <div className="card col-sm-12 col-md-10">
+        <h1 className="bf text-center m-4">
+          <u>Page Under Maintenance</u>
+        </h1>
+      </div>
+    </div>
+  );
+}
+export default Maintenance;
